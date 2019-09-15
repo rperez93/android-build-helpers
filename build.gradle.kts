eb72e7a4ba34148afba1fd4c575d982f12b3264f
Rafael Pérez <perez.rafael1993@gmail.com>
2019-09-15 13:00:20 -0500

    * Updated dependencies * Updated pom file descriptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,23 @@
+import groovy.lang.Closure
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.kotlin.dsl.kotlin
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+    dependencies {
+        classpath("com.bmuschko:gradle-nexus-plugin:2.3.1")
+    }
+}
 
 plugins {
     java
     maven
+    `maven-publish`
     kotlin("jvm") version "1.3.50"
+    id("com.bmuschko.nexus") version "2.3.1"
 }
-
-
 
 group = "com.github.rperez93"
 version = "1.0.0"
@@ -16,6 +27,7 @@ repositories {
     google()
     jcenter()
 }
+
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
@@ -32,18 +44,35 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-tasks.getByName<Upload>("uploadArchives") {
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("My Library")
+                description.set("A concise description of my library")
+                url.set("http://www.example.com/library")
 
-    repositories {
-
-        withConvention(MavenRepositoryHandlerConvention::class) {
-
-            mavenDeployer {
-                println(mavenLocal().url)
-                withGroovyBuilder {
-                    "repository"("url" to uri(mavenLocal().url))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("johnd")
+                        name.set("John Doe")
+                        email.set("john.doe@example.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://example.com/my-library.git")
+                    developerConnection.set("scm:git:ssh://example.com/my-library.git")
+                    url.set("http://example.com/my-library/")
                 }
             }
         }
     }
 }
+
+apply(from = "nexus.gradle")
